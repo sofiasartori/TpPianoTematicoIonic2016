7093aa7a8b90dbf8df556554904b351372424639
Sofia Sartori <Sofia Sartori>
2016-09-23 18:31:24 -0300

    Tuve que rehacer el proyecto en formato tabs, tiraba error en sideMenu, sigue sin funcionar nativeAudio

--- pianoSartori/www/js/app.js
+++ pianoSartori/www/js/app.js
@@ -3,14 +3,15 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
@@ -22,43 +23,64 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $ionicConfigProvider.tabs.position('bottom');
   $stateProvider
 
-    .state('app', {
-    url: '/app',
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
     abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+    templateUrl: 'templates/tabs.html'
   })
 
-  .state('app.piano', {
-    url: '/piano',
+  // Each tab has its own nav history stack:
+
+  .state('tab.miCuenta', {
+    url: '/miCuenta',
     views: {
-      'menuContent': {
-        templateUrl: 'templates/piano.html',
-        controller: 'PianoCtrl'
+      'tab-miCuenta': {
+        templateUrl: 'templates/miCuenta.html',
+        controller: 'MiCuentaCtrl'
       }
     }
   })
 
-  .state('app.melodias', {
-      url: '/misMelodias',
+  .state('tab.piano', {
+      url: '/piano',
       views: {
-        'menuContent': {
-          templateUrl: 'templates/misMelodias.html'
+        'tab-piano': {
+          templateUrl: 'templates/piano.html',
+          controller: 'PianoCtrl'
         }
       }
-  })
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
 
-  .state('app.acercaDe',{
-      url:'/acercaDe',
-      views:{
-        'menuContent':{
-          templateUrl: 'templates/acercaDe.html'
+  .state('tab.acercaDe', {
+    url: '/acercaDe',
+    views: {
+      'tab-acercaDe': {
+        templateUrl: 'templates/acercaDe.html',
+        controller: 'AcercaDeCtrl'
       }
     }
   });
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/piano');
+  $urlRouterProvider.otherwise('/tab/piano');
+
 });
--- pianoSartori/www/js/controllers.js
+++ pianoSartori/www/js/controllers.js
@@ -1,75 +1,21 @@
-angular.module('starter.controllers', ['ionic', 'ngCordova'])
+angular.module('starter.controllers', ['ionic'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('MiCuentaCtrl', function($scope) {})
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-.controller('PianoCtrl', function($scope, $cordovaVibration, $cordovaNativeAudio) {
-  $cordovaNativeAudio.preloadSimple('sonido1', 'sounds/1.mp3');
-  $cordovaNativeAudio.preloadSimple('sonido2', 'sounds/2.mp3');
-  $cordovaNativeAudio.preloadSimple('sonido3', 'sounds/3.mp3');
-  $cordovaNativeAudio.preloadSimple('sonido4', 'sounds/4.mp3');
-  $cordovaNativeAudio.preloadSimple('sonido5', 'sounds/5.mp3');
-  $cordovaNativeAudio.preloadSimple('sonido6', 'sounds/6.mp3');
-
+.controller('PianoCtrl', ['$scope', '$state', function ($scope, $state) {
+  //$cordovaNativeAudio.preloadSimple("sonido1", "sounds/sonido1.mp3");
   $scope.boton1 = function(){
-    $cordovaVibration.vibrate(1000);
-    $cordovaNativeAudio.play('sonido1');
+    console.log("hola");
+    //$cordovaNativeAudio.play("sonido1");
   }
-  $scope.boton2 = function(){
-    $cordovaVibration.vibrate(1000);
-    $cordovaNativeAudio.play('sonido2');
-  }
-  $scope.boton3 = function(){
-    $cordovaVibration.vibrate(1000);
-    $cordovaNativeAudio.play('sonido3');
-  }
-  $scope.boton4 = function(){
-    $cordovaVibration.vibrate(1000);
-    $cordovaNativeAudio.play('sonido4');
-  }
-  $scope.boton5 = function(){
-    $cordovaVibration.vibrate(1000);
-    $cordovaNativeAudio.play('sonido5');
-  }
-  $scope.boton6 = function(){
-    $cordovaVibration.vibrate(1000);
-    $cordovaNativeAudio.play('sonido6');
-  }
+}])
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
 })
+
+.controller('AcercaDeCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
