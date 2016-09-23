df01366e4b464c13de49e3823ef4434416a16529
Sofia Sartori <Sofia Sartori>
2016-09-23 20:33:50 -0300

    Front end hecho

--- pianoSartori/www/js/controllers.js
+++ pianoSartori/www/js/controllers.js
@@ -1,6 +1,41 @@
 angular.module('starter.controllers', ['ionic'])
+.factory('login', function () {
+    var logueado = false;
+    var login = {};
+    login.setLogueado = function (valor) {
+      logueado = valor;
+    };
+    login.getLogueado = function () {
+      return logueado;
+    };
+    return login;
+  })
+  .controller("FooterCtrl", ['$scope', '$rootScope', 'login', function ($scope, $state, login) {
+    $scope.getLogueado = login.getLogueado;
+  }])
+  .controller('MiCuentaCtrl', ['$scope', '$state', 'login', "$ionicPopup", function ($scope, $state, login, $ionicPopup) {
+    $scope.getLogueado = login.getLogueado;
+    $scope.setLogueado = login.setLogueado;
+    $scope.form = {}
+    $scope.error = false;
+    $scope.form.usuario = "";
+    $scope.showAlert = function () {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Piano Cat',
+        template: 'Ingresa con un usuario'
+      });
+    }
+    $scope.Ingresar = function () {
+      console.log("usuario: " + $scope.form.usuario);
+      if ($scope.form.usuario != "") {
+        $scope.setLogueado(true);
+        $state.go('tab.piano');
+      } else {
+        $scope.showAlert();
+      }
+    }
 
-.controller('MiCuentaCtrl', function($scope) {})
+  }])
 
 .controller('PianoCtrl', ['$scope', '$state', function ($scope, $state) {
   //$cordovaNativeAudio.preloadSimple("sonido1", "sounds/sonido1.mp3");
